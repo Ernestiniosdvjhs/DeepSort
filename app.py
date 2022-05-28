c120ec1ae07d64367ff2931d1b8cf3ca89882e33
Tsetsen-Kedyaev <lineked@yandex.ru>
2022-05-28 20:00:14 +0300

    second commit

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import sys
 from flask import *
 from werkzeug.utils import secure_filename
 import time
+import psycopg2
 
 UPLOAD_FOLDER = '/home/lineked/PycharmProjects/deepsort_project/uploads'
 FOLDER_WIT_FILE = '/home/lineked/PycharmProjects/deepsort_project/runs/track'
@@ -10,11 +11,45 @@ ALLOWED_EXTENSIONS = {'mp4'}#Расширения файлов, которые �
 
 app = Flask(__name__)
 app.config['SECRET_KEY']= 'oifeeowwffwefew342'
+conn = psycopg2.connect(
+        host="localhost",
+        database="db_track",
+        user="admindb",
+        password="pass",
+        port="5432"
+)
+cur = conn.cursor()
+cur.execute('DROP TABLE IF EXISTS data;')
+cur.execute('CREATE TABLE data (id SERIAL NOT NULL,'
+                               'filename VARCHAR NOT NULL,'
+                               'duration_video VARCHAR NOT NULL);'
+                            )
 
 
 @app.route('/', methods=['GET'])
 def index():
     return redirect("/main")
+
+def send_file(file, filename):
+    file.save(os.path.join(UPLOAD_FOLDER, filename))
+    os.system(f'python3 track.py --source uploads/{filename} --yolo_model weights.pt --save-vid --save-txt')
+    os.remove(f'uploads/{filename}')
+    get_files = os.listdir(FOLDER_WIT_FILE)
+    print(get_files.sort())
+    FOLDER_WITH_FILE = f"/home/lineked/PycharmProjects/deepsort_project/runs/track/{get_files[-1]}"
+    app.config['FOLDER_WITH_FILE'] = FOLDER_WITH_FILE
+    print(FOLDER_WITH_FILE)
+    time.sleep(30)
+    cur.execute('INSERT INTO data (filename, duration_video) '
+                'VALUES (%s, %s)',
+                ('video1',
+                 'test')
+                )
+    conn.commit()
+    cur.close()
+    conn.close()
+    return send_from_directory(app.config['FOLDER_WITH_FILE'], filename, as_attachment=True)
+
 
 @app.route('/main', methods=['POST', 'GET'])
 def page_main():
@@ -27,22 +62,12 @@ def page_main():
         if file:
             filename = secure_filename(file.filename)
             if filename.split('.')[1] not in ALLOWED_EXTENSIONS:
-                flash('Неправильный тип файла. Пожалуйста, выберите файл формата .mp4', category='error')
+                flash('Неправильный формат файла. Пожалуйста, выберите файл формата .mp4', category='error')
             else:
-                file.save(os.path.join(UPLOAD_FOLDER, filename))
-                os.system(f'python3 track.py --source uploads/{filename} --yolo_model weights.pt --save-vid')
-                os.remove(f'uploads/{filename}')
-                get_files = os.listdir(FOLDER_WIT_FILE)
-                print(get_files.sort())
-                FOLDER_WITH_FILE = f"/home/lineked/PycharmProjects/deepsort_project/runs/track/{get_files[-1]}"
-                app.config['FOLDER_WITH_FILE'] = FOLDER_WITH_FILE
-                print(FOLDER_WITH_FILE)
-                time.sleep(30)
-                return send_from_directory(app.config['FOLDER_WITH_FILE'], filename, as_attachment=True)
+                return send_file(file, filename)
         else:
-            flash("Нет выбранного файла", category='error')
+            flash("Нет выбранного файла", category='nofile')
     return render_template('main.html')
-
 
 @app.route('/info/', methods=['GET', 'POST'])
 def page_about_us():
@@ -51,7 +76,7 @@ def page_about_us():
             return redirect("/main")
         elif request.form.get("info"):
             return redirect("/info/")
-    return render_template('main.html')
+    return render_template('info.html')
 
 
 if __name__ == '__main__':
