f8b3bdc71e97a52a9af2256ac8e0920a93faaf93
Tsetsen-Kedyaev <lineked@yandex.ru>
2022-05-30 11:37:24 +0300

    update file

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from werkzeug.utils import secure_filename
 import time
 import count_detect
 
-UPLOAD_FOLDER = '/home/lineked/PycharmProjects/deepsort_project/uploads'
-RESULTS = '/home/lineked/PycharmProjects/deepsort_project/results'
-FOLDER_WITH_FILE = '/home/lineked/PycharmProjects/deepsort_project/runs/track'
+UPLOAD_FOLDER = f'{os.path.join(os.path.dirname(__file__))}/uploads/'
+RESULTS = f'{os.path.join(os.path.dirname(__file__))}/results'
+FOLDER_WITH_FILE = f'{os.path.join(os.path.dirname(__file__))}/runs/track'
 ALLOWED_EXTENSIONS = {'mp4'}#Расширения файлов, которые можно загрузить
 
 app = Flask(__name__)
@@ -24,8 +24,8 @@ def download(file, filename):#загрузка файла
     os.remove(f'uploads/{filename}')
     if len(os.listdir(RESULTS))>0:
         os.system(f'rm -rf {RESULTS}/*')
-    FILE = f'/home/lineked/PycharmProjects/deepsort_project/runs/track/weights_osnet_x0_25/{filename}'
-    TXTFILE = f'/home/lineked/PycharmProjects/deepsort_project/runs/track/weights_osnet_x0_25/tracks/{filename[:-4]}.txt'
+    FILE = f'{os.path.join(os.path.dirname(__file__))}/runs/track/weights_osnet_x0_25/{filename}'
+    TXTFILE = f'{os.path.join(os.path.dirname(__file__))}/runs/track/weights_osnet_x0_25/tracks/{filename[:-4]}.txt'
     time.sleep(20)
     os.system(f'cp {FILE} {RESULTS}')
     os.system(f'cp {TXTFILE} {RESULTS}')
